0d662d9682fe5620d2edc3c887b123e28b9de62e
orazi-paolo <orazi.paolo10@gmail.com>
2024-08-23 16:58:36 +0200

    added milestone 2 && 3

--- js/script.js
+++ js/script.js
@@ -4,12 +4,6 @@ L'utente clicca su un bottone che genererà una griglia di gioco quadrata.
 Ogni cella ha un numero progressivo, da 1 a 100.
 Ci saranno quindi 10 caselle per ognuna delle 10 righe.
 Quando l'utente clicca su ogni cella, la cella cliccata si colora di azzurro ed emetto un messaggio in console con il numero della cella cliccata.
-# MILESTONE 1
-Prepariamo l'HTML ed il CSS per ottenere il risultato grafico che vediamo nell'immagine allegata.
-#MILESTONE 2
-Rimuoviamo le celle che abbiamo inserito nell'HTML in modo da generarle tramite JS. Al click del bottone play, vengono generate 100 celle in 10 righe da 10 celle ciascuna.
-#MILESTONE 3
-In ogni cella, deve comparire il numero corrispondente, in ordine da 1 a 100;
 #MILESTONE 4
 Al click sulla cella, stampiamo il numero della cella cliccata in console, poi coloriamo la cella d'azzurro!
 # BONUS
@@ -19,4 +13,19 @@ Aggiungere una select accanto al bottone di generazione, che fornisca una scelta
 - con difficoltà 3 => 49 caselle, con un numero compreso tra 1 e 49, divise in 7 caselle per 7 righe; 
 */
 
-// MILESTONE 1  
+// MILESTONE 1 OK
+
+// #MILESTONE 2 && MILESTONE 3 OK
+// prendo dal dom gli elementi che mi servono
+const buttonPlay = document.getElementById('play');
+const grid = document.getElementById('grid');
+
+// quando clicco su play genero le celle con dentro i numeri
+buttonPlay.addEventListener('click', function (){
+    for(let i = 1; i <= 100; i++){
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.innerText = `${i}`;
+        grid.appendChild(cell);
+    }
+})
